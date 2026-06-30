098cee04714094c8729bfb582434fb3c1f3902c1
rterminus <luizrenanpontes@gmail.com>
2026-06-30 19:10:50 -0300

    feat: navbar finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,21 @@
+import { Search } from "lucide-react";
+
 export default function Navbar() {
   return (
-    <header className="bg-surface border-b-outline-variant flex w-full flex-row items-center justify-between border-b">
-      <h1 className="text-primary font-mono text-2xl font-bold tracking-wider">
-        GITEXPLORER
-      </h1>
-      <div className=""></div>
-      <div className=""></div>
+    <header className="bg-surface border-b-outline w-full border-b">
+      <div className="mx-auto flex h-20 w-full max-w-3xl flex-row items-center justify-between px-6 lg:px-0">
+        <h1 className="text-primary font-mono text-2xl font-bold tracking-wider">
+          GITEXPLORER
+        </h1>
+        <div className="relative w-full max-w-md">
+          <Search className="text-outline absolute top-1/2 left-3 h-5 w-5 -translate-y-1/2" />
+          <input
+            className="bg-surface-input text-outline border-outline-variant focus:ring-primary focus:border-primary w-full rounded-md border px-4 py-2 pl-10 font-bold transition-colors focus:ring-1 focus:outline-none"
+            type="text"
+            placeholder="Search GitHub username..."
+          ></input>
+        </div>
+      </div>
     </header>
   );
 }
