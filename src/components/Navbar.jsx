bbbc208656d859f0fffd40975fe1d7f4373126a8
rterminus <luizrenanpontes@gmail.com>
2026-06-30 18:27:44 -0300

    feat: tailwind global styling setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <header className="bg-surface border-b-outline-variant flex w-full flex-row items-center justify-between border-b">
+      <h1 className="text-primary font-mono text-2xl font-bold tracking-wider">
+        GITEXPLORER
+      </h1>
+      <div className=""></div>
+      <div className=""></div>
+    </header>
+  );
+}
